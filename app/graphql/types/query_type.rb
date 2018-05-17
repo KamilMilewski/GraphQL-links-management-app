8c053c79708f122c8d8ee699b49842e54bc0d45a
Kamil Milewski <kamil.milewski@hotmail.com>
2018-05-17 16:21:48 +0200

    implement link field

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,6 +4,13 @@ Types::QueryType = GraphQL::ObjectType.define do
     resolve -> (obj, args, ctx) { Link.all }
   end
 
+  field :link, Types::LinkType do
+    argument :id, types.Int
+    resolve -> (obj, args, ctx) do
+       Link.find(args[:id])
+     end
+  end
+
   field :allUsers, !types[Types::UserType] do
     resolve -> (obj, args, ctx) { User.all }
   end
